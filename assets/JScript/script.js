78bc8f2bff5aaee0a1d43840d48c23bc0fbf6530
Chris Onions <c.onions101@gmail.com>
2021-03-10 12:09:10 +1100

    added randomiser for the choices array added output

--- assets/JScript/script.js
+++ assets/JScript/script.js
@@ -1,172 +1,174 @@
 // On button press 
 
-  // prompt for an input of length of password
+// prompt for an input of length of password
 // "how long would you like your password to be 8 - 100 characters"
 
-  // prompted for character types I choose lowercase, uppercase, numeric, and/or special character
+// prompted for character types I choose lowercase, uppercase, numeric, and/or special character
 // "would you like Numbers?" user Input
 // "would you like uppercase letters?" user input yes or no
 // "would you like special Characters?"user input yes or no
 
-  var password = " ";
-    //String of uppercase letters
-  var upperCaseLetters  = "ABCDEFGHIJKLMNOPQRXTUVWXYZ" ;
-    //split the letters into an individual array
-  var upperCaseLetters = upperCaseLetters.split("");
-    //String of lowercase letters
-  var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz" ;
-    //split the letters into an individual array
-  var lowerCaseLetters = lowerCaseLetters.split("");
-    // String of all special characters
-  var specialCharacters = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~" ;
-    //split the Special characters into an individual array
-  var specialCharacters = specialCharacters.split("");
-  var numberLengthNumber = 0
-  var numbersChoiceBool = null
-  var caseChoiceBool = null
-  var letterChoiceBool = null
-  var specialCharBool = null 
-  
+var password = "";
+//String of uppercase letters
+var upperCaseLetters = "ABCDEFGHIJKLMNOPQRXTUVWXYZ";
+//split the letters into an individual array
+upperCaseLetters = upperCaseLetters.split("");
+//String of lowercase letters
+var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+//split the letters into an individual array
+lowerCaseLetters = lowerCaseLetters.split("");
+// String of all special characters
+var specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+//split the Special characters into an individual array
+specialCharacters = specialCharacters.split("");
+// 
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+
+var numberLengthNumber;
+// var numbersChoiceBool = null
+// var caseChoiceBool = null
+// var letterChoiceBool = null
+// var specialCharBool = null
+
 
 
 
 // Function to ask questions for password generation
-function generatePassword(){
+function generatePassword() {
   // user input for password length  
-  var numberLengthNumber = parseInt(
+  numberLengthNumber = parseInt(
     prompt("How long would you like your password to be? 8-100"));
-    console.log(numberLengthNumber);
+  console.log(numberLengthNumber);
 
 
   // Checks if the number is a number and not a sting
   if (isNaN(numberLengthNumber) === !true) {
     console.log("correct");
-    }
+  }
   else {
-    alert ("sorry please enter a NUMBER and try again")
-  return;
+    alert("sorry please enter a NUMBER and try again")
+    return;
   }
 
   // checks if number below secified value 
-  if (numberLengthNumber <= 100 ) {
+  if (numberLengthNumber <= 100) {
     console.log("correct");
   }
   else {
     alert("Sorry " + numberLengthNumber + " is not between 8-100")
-  return;
-  } 
+    return;
+  }
 
   // checks if number above secified value
-  if (numberLengthNumber > 8) {
+  if (numberLengthNumber >= 8) {
     console.log("correct");
   }
   else {
     alert("Sorry " + numberLengthNumber + " is not between 8-100 try again")
-  return;}
-
-// User input For if they want uppercase letters 
+    return;
+  }
+  var choicesArray = [];
+  // User input For if they want uppercase letters 
   var numbersChoiceBool = confirm("Press ok for Numbers in your password if not press Cancel")
-  if (numbersChoiceBool == true){
-      numbersChoiceBool = true ;
-  console.log(numbersChoiceBool);
+  if (numbersChoiceBool == true) {
+    choicesArray = choicesArray.concat(numbers)
+    console.log(numbersChoiceBool);
   }
-  else {
-    numbersChoiceBool = false
-    console.log(numbersChoiceBool)
-  }
+  // else {
+  //   numbersChoiceBool = false
+  //   console.log(numbersChoiceBool)
+  // }
 
   //Ask user if they want letter aswell as numbers
-  var  userChoiceArray = confirm("Press OK if you would you like to also use Letters if not Cancel")
-    if (userChoiceArray == true){
-        userChoiceArray = true 
-    console.log( userChoiceArray);
-    }
-    else {
-      userChoiceArray[0] = false;
-      console.log(userChoiceArray)
-    }
-    console.log(userChoiceArray)
+  var userChoiceBool = confirm("Press OK if you would you like to use Lower Case Letters if not Cancel")
+  if (userChoiceBool == true) {
+    choicesArray = choicesArray.concat(lowerCaseLetters)
+    console.log(userChoiceBool);
+  }
+  // else {
+  //   userChoiceArray[0] = false;
+  //   console.log(userChoiceArray)
+  // }
   // Ask user if they want to use upper and lower case
   var caseChoiceBool = confirm("Press ok for Both UPPER and lower case if you would like only lower case press Cancel")
-    if (caseChoiceBool == true){
-        caseChoiceBool = true 
-    
+  if (caseChoiceBool == true) {
+    choicesArray = choicesArray.concat(upperCaseLetters)
     console.log(caseChoiceBool);
-    }
-    else {
-    caseChoiceBool = false
-      console.log(caseChoiceBool)
-    }
-    //Asks user if they want Special chars
-  var specialCharBool = confirm("Press ok for Both UPPER and lower case if you would like only lower case press Cancel")
-    if (specialCharBool == true){
-        specialCharBool = true 
-
-    console.log(specialCharBool);
-    }
-    else {
-      specialCharBool = false
-      console.log(specialCharBool)
-    }
-
-    if  (
-      numbersChoiceBool === false && 
-      caseChoiceBool === false && 
-      letterChoiceBool === false && 
-      specialCharBool === false  
-      ) {
-        alert ("You need to choose at least one Try again" )
-        generatePassword()
-        console.log("no options selected")
-        } 
-    
-  // 
-
-
   }
+  // else {
+  //   caseChoiceBool = false
+  //   console.log(caseChoiceBool)
+  // }
+  //Asks user if they want Special chars
+  var specialCharBool = confirm("Press ok for Special Characters if not  press Cancel")
+  if (specialCharBool == true) {
+    choicesArray = choicesArray.concat(specialCharacters)
+    console.log(specialCharBool);
+  }
+  // else {
+  //   specialCharBool = false
+  //   console.log(specialCharBool)
+  // }
+
+  if (
+    numbersChoiceBool === false &&
+    caseChoiceBool === false &&
+    userChoiceBool === false &&
+    specialCharBool === false
+  ) {
+    alert("You need to choose at least one Try again")
+    console.log("no options selected")
+    return;
+  }
+  console.log(choicesArray)
+  for (var i = 0; i < numberLengthNumber; i++) {
+    var item = choicesArray[Math.floor(Math.random() * choicesArray.length)];
+    password += item;
+  }
+  return password;
+}
+// create an array that has a specified ammout of empty elements
+
+// choose a random element in an array of the chosen size 
+
+// asign the random element chosen from the element chooser a random choiceBool value of the chosen types
+// concat the array
+// Win
 
 
 
 
 
-
- 
-
-// Make an array of all characters. A - Z
 // Make a random number generator in an function. 0 - 9
+
 // Make an array to hold the password values (total output)
 
-// Make a function to choose how long the password will be 8 - 100
-// Make a make a function to choose each element from either the letters or numbers randomly 
-// Add them all to the array at the end 
+// Make a function to choose each element from either the letters or numbers randomly 
+
 // consolidate the array so its printed as a password
 
-// Generate password button clicked
 
-
-//  The password is either displayed in an alert or written to the page
-
-// prompted for character types I choose lowercase, uppercase, numeric, and/or special character
-
-
-
-// Assignment Code
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
+// function completeGeneration() {
+//   generatePassword()
+//   createSizeArray()
 // }
 
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+
+
+var generateBtn = document.querySelector("#generate");
 
 
 
+// Write password to the #password input
+function writePassword() {
+  // var arrayGen = createSizeArray();
+  var password = generatePassword();
 
-// Resourses
+  var passwordText = document.querySelector("#password");
 
+  passwordText.value = password;
+
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
