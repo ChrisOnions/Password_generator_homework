0e006d4ec26e24837163a614bb7cc3b6978da600
Chris Onions <c.onions101@gmail.com>
2021-03-11 11:38:50 +1100

    Removed all the redundant else statements from the input confirm if statements

--- assets/JScript/script.js
+++ assets/JScript/script.js
@@ -1,13 +1,3 @@
-// On button press 
-
-// prompt for an input of length of password
-// "how long would you like your password to be 8 - 100 characters"
-
-// prompted for character types I choose lowercase, uppercase, numeric, and/or special character
-// "would you like Numbers?" user Input
-// "would you like uppercase letters?" user input yes or no
-// "would you like special Characters?"user input yes or no
-
 var password = "";
 //String of uppercase letters
 var upperCaseLetters = "ABCDEFGHIJKLMNOPQRXTUVWXYZ";
@@ -21,18 +11,10 @@ lowerCaseLetters = lowerCaseLetters.split("");
 var specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 //split the Special characters into an individual array
 specialCharacters = specialCharacters.split("");
-// 
+// Numbers variable 
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-
-
+// vadiable to store the number length
 var numberLengthNumber;
-// var numbersChoiceBool = null
-// var caseChoiceBool = null
-// var letterChoiceBool = null
-// var specialCharBool = null
-
-
-
 
 // Function to ask questions for password generation
 function generatePassword() {
@@ -40,7 +22,6 @@ function generatePassword() {
   numberLengthNumber = parseInt(
     prompt("How long would you like your password to be? 8-100"));
   console.log(numberLengthNumber);
-
 
   // Checks if the number is a number and not a sting
   if (isNaN(numberLengthNumber) === !true) {
@@ -51,7 +32,7 @@ function generatePassword() {
     return;
   }
 
-  // checks if number below secified value 
+  // Checks if number below secified value 
   if (numberLengthNumber <= 100) {
     console.log("correct");
   }
@@ -75,42 +56,26 @@ function generatePassword() {
     choicesArray = choicesArray.concat(numbers)
     console.log(numbersChoiceBool);
   }
-  // else {
-  //   numbersChoiceBool = false
-  //   console.log(numbersChoiceBool)
-  // }
-
-  //Ask user if they want letter aswell as numbers
+ 
   var userChoiceBool = confirm("Press OK if you would you like to use Lower Case Letters if not Cancel")
   if (userChoiceBool == true) {
     choicesArray = choicesArray.concat(lowerCaseLetters)
     console.log(userChoiceBool);
   }
-  // else {
-  //   userChoiceArray[0] = false;
-  //   console.log(userChoiceArray)
-  // }
-  // Ask user if they want to use upper and lower case
+
   var caseChoiceBool = confirm("Press ok for Both UPPER and lower case if you would like only lower case press Cancel")
   if (caseChoiceBool == true) {
     choicesArray = choicesArray.concat(upperCaseLetters)
     console.log(caseChoiceBool);
   }
-  // else {
-  //   caseChoiceBool = false
-  //   console.log(caseChoiceBool)
-  // }
-  //Asks user if they want Special chars
+ 
   var specialCharBool = confirm("Press ok for Special Characters if not  press Cancel")
   if (specialCharBool == true) {
     choicesArray = choicesArray.concat(specialCharacters)
     console.log(specialCharBool);
   }
-  // else {
-  //   specialCharBool = false
-  //   console.log(specialCharBool)
-  // }
 
+  //
   if (
     numbersChoiceBool === false &&
     caseChoiceBool === false &&
@@ -120,6 +85,7 @@ function generatePassword() {
     alert("You need to choose at least one Try again")
     console.log("no options selected")
     return;
+    //
   }
   console.log(choicesArray)
   for (var i = 0; i < numberLengthNumber; i++) {
@@ -128,37 +94,8 @@ function generatePassword() {
   }
   return password;
 }
-// create an array that has a specified ammout of empty elements
-
-// choose a random element in an array of the chosen size 
-
-// asign the random element chosen from the element chooser a random choiceBool value of the chosen types
-// concat the array
-// Win
-
-
-
-
-
-// Make a random number generator in an function. 0 - 9
-
-// Make an array to hold the password values (total output)
-
-// Make a function to choose each element from either the letters or numbers randomly 
-
-// consolidate the array so its printed as a password
-
-
-// function completeGeneration() {
-//   generatePassword()
-//   createSizeArray()
-// }
-
-
 
 var generateBtn = document.querySelector("#generate");
-
-
 
 // Write password to the #password input
 function writePassword() {
